9d6829424ae7edcb001051f5cddb730ef9681a0f
Clark Rinker <cgrinker@gmail.com>
2017-03-17 12:17:37 -0700

    Fixing Webpack.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+console.log(AppContainer);
+function render() {
+  ReactDOM.render((
+  <AppContainer>
+    <div>FFFF</div>
+  </AppContainer>
+), document.getElementById('react-root'));
+}
+render();
 
-export default {
-
+if (module.hot) {
+  module.hot.accept('./', render)
+  module.hot.accept('./modules', render);
 }
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+
+  entry: './src/index.js',
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    publicPath: 'static',
+    library: 'yarl',
+  },
+
+  devServer: {
+    hot: true,
+    contentBase: path.resolve(__dirname, 'static'),
+    publicPath: '/'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|woff2|svg|ttf|woff|eot)(\?.*)?$/,
+        loader: "url",
+        exclude: /node_modules/
+      },
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+  ],
+
+}
